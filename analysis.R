97768944f934393da6d381145d84cc571e098e71
thomas.lapiana <tal103020@icloud.com>
2016-05-03 16:06:48 -0500

    started solving third problem

--- analysis.R
+++ analysis.R
@@ -2,9 +2,13 @@
 setwd("~/JHU DataScience Courses/R Directory/RepData_PeerAssessment1")
 library(data.table)
 library(dplyr)
+library(Amelia)
 
 ## Load in the data
 activity = data.table(read.csv('activity.csv'))
+
+## FIll in the missing values for later use
+activity = 
 
 ## Group the data by day
 grouped_day = activity %>% filter(!is.na(steps)) %>% group_by(date) %>% summarise_each(funs(sum)) %>% select(date,steps)
@@ -18,6 +22,11 @@ print(c(Mean,Median))
 ## Group the data by 5-minute interval
 grouped_interval = activity %>% filter(!is.na(steps)) %>% group_by(interval) %>% summarise_each(funs(mean)) %>% select(interval,steps)
 
+## Perform Step 2 Analysis
 plot(grouped_interval$interval,grouped_interval$steps,
      type = 'l')
 head(arrange(grouped_interval,desc(steps)),1)
+
+## Perform Step 3 Analysis 
+print(sum(is.na(activity$steps)))
+imputed_activity = 
